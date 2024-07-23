b297bc5df11bcca81ae0de63e35a06f9a65e48cb
Sina <sinaa1383@gmail.com>
2024-07-23 15:02:19 +0330

    feat: Add dependencies

--- app/src/main/java/xyz/sina/clevercapitalist/MainActivity.kt
+++ app/src/main/java/xyz/sina/clevercapitalist/MainActivity.kt
@@ -19,29 +19,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CleverCapitalistTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CleverCapitalistTheme {
-        Greeting("Android")
-    }
-}
