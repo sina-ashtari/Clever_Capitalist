3e48b2ac0b92466ef5b58257103cb7bd4319eece
Sina <sinaa1383@gmail.com>
2024-07-23 16:17:33 +0330

    feat: Add Main UI

--- app/src/main/java/xyz/sina/clevercapitalist/MainActivity.kt
+++ app/src/main/java/xyz/sina/clevercapitalist/MainActivity.kt
@@ -4,13 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import view.MainView
+import view.SignUp
 import xyz.sina.clevercapitalist.ui.theme.CleverCapitalistTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +14,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CleverCapitalistTheme {
-
+                SignUp()
             }
         }
     }
