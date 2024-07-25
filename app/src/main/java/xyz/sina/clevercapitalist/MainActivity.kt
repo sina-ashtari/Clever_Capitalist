b14e379f44541ad353b0578312ce2d2f47a96309
Sina <sinaa1383@gmail.com>
2024-07-25 18:44:09 +0330

    Update

--- app/src/main/java/xyz/sina/clevercapitalist/MainActivity.kt
+++ app/src/main/java/xyz/sina/clevercapitalist/MainActivity.kt
@@ -4,17 +4,32 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import view.MainView
-import view.SignUp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.compose.rememberNavController
+import dagger.hilt.android.AndroidEntryPoint
+import viewModel.authentication.AuthViewModel
+import androidx.compose.runtime.collectAsState
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import xyz.sina.clevercapitalist.ui.theme.CleverCapitalistTheme
 
+
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             CleverCapitalistTheme {
-                SignUp()
+                val navController = rememberNavController()
+                val authViewModel: AuthViewModel = hiltViewModel()
+                val isLoggedIn = authViewModel.authState.value
+
+                NavHost(navController = navController, startDestination = if (isLoggedIn == true) "Main" else "login" ){
+                    composable("login") {  }
+                    composable("dashboard") {  }
+                }
             }
         }
     }
