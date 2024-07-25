2230d84d8eb521c1bdc921ad79279b1ea182586c
Sina <sinaa1383@gmail.com>
2024-07-25 17:12:12 +0330

    Add : Firebase Authentication Module for Hilt

---
+++ app/src/main/java/model/FireBaseModule.kt
@@ -0,0 +1,18 @@
+package model
+
+import com.google.firebase.auth.FirebaseAuth
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object FireBaseModule {
+    @Provides
+    @Singleton
+    fun provideFirebaseAuth(): FirebaseAuth{
+        return FirebaseAuth.getInstance()
+    }
+}
