d1b05ef5b24f1bb52d261aca23f5184cdeb8fdc8
Sina <sinaa1383@gmail.com>
2024-07-28 21:31:00 +0330

    feat : Added Dashboard route

--- app/src/main/java/xyz/sina/clevercapitalist/view/Routes.kt
+++ app/src/main/java/xyz/sina/clevercapitalist/view/Routes.kt
@@ -4,4 +4,5 @@ object Routes {
     const val SIGN_UP_ROUTE: String = "SignUpView"
     const val SIGN_IN_ROUTE: String = "SignInView"
     const val MAIN_ROUTE : String  = "MainView"
+    const val DASHBOARD_ROUTE : String = "Dashboard"
 }
