13f870f46df3d90847e57616559d5c35afa470c2
Sina <sinaa1383@gmail.com>
2024-07-29 19:11:58 +0330

    feat : added more info gathering

--- app/src/main/java/xyz/sina/clevercapitalist/view/RegisterForm.kt
+++ app/src/main/java/xyz/sina/clevercapitalist/view/RegisterForm.kt
@@ -3,19 +3,25 @@ package xyz.sina.clevercapitalist.view
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.pager.HorizontalPager
-import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material.Button
+import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
-import androidx.compose.material3.Checkbox
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.outlined.Check
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -34,13 +40,13 @@ fun RegisterForm(){
                 when(page){
                     0 -> {NameGetter()}
                     1 -> {HomeStatus()}
-                    2 -> {}
-                    3 -> {}
+                    2 -> {TravelStatus()}
+                    3 -> {RegularlySpend()}
                     4 -> {}
                 }
             }
 
-            if(pagerState.currentPage < pagerState.currentPage -1){
+            if(pagerState.currentPage < pagerState.pageCount -1){
                 Button(onClick = { scope.launch { pagerState.animateScrollToPage(pagerState.currentPage+1) } }) {
                     Text(text = "Next")
                 }
@@ -66,43 +72,83 @@ fun NameGetter() {
 }
 @Composable
 fun HomeStatus(){
-    // i should do this with repeat of course ..
-    val checkState = remember { mutableStateOf(false) }
-    val checkState1 = remember { mutableStateOf(false) }
-    val checkState2 = remember { mutableStateOf(false) }
+
+    val btnTexts = listOf(
+        "I rent",
+        "I own",
+        "Other"
+    )
+    var selectedIndex by remember { mutableStateOf(-1) }
+
 
     Column {
         Text("Tell us about your home")
 
-        Button(onClick = {
-            checkState.value = true
-            checkState1.value = false
-            checkState2.value = false
-        }, border = BorderStroke(1.dp , if (checkState.value) Color.Red else Color.Black)) {
-            Text(text = "I rent")
-            Checkbox(checked = checkState.value, onCheckedChange = {} )
-        }
-        Button(onClick = {
-            checkState.value = false
-            checkState1.value = true
-            checkState2.value = false
-        }, border = BorderStroke(1.dp , if (checkState1.value) Color.Red else Color.Black)) {
-            Text(text = "I own")
-            Checkbox(checked = checkState1.value, onCheckedChange = {} )
-        }
-        Button(onClick = {
-            checkState.value = false
-            checkState1.value = false
-            checkState2.value = true
-        }, border = BorderStroke(1.dp , if (checkState2.value) Color.Red else Color.Black)) {
-            Text(text = "Other")
-            Checkbox(checked = checkState2.value, onCheckedChange = {} )
+        repeat(btnTexts.size){index ->
+            Row(){
+                Button(onClick = {selectedIndex = index}, border = BorderStroke(1.dp ,  if(selectedIndex == index) Color.Red else Color.Black)) {
+                    Text(text = btnTexts[index])
+                    Icon(imageVector = if(selectedIndex == index) Icons.Default.Check else Icons.Outlined.Check , contentDescription = null )
+                }
+            }
+
         }
 
     }
 }
 @Composable
 fun TravelStatus(){
+
+    val checkState = remember {
+        mutableStateListOf(false,false,false,false,false,false)
+    }
+    val btnTexts = listOf(
+        "Car",
+        "Public transit",
+        "Rideshare (Uber/Lyft/etc.)",
+        "Motorcycle",
+        "Bike",
+        "Walk"
+    )
+
+    Column {
+        Text("How do you get around?")
+        repeat(btnTexts.size){index ->
+            Row{
+                Button(onClick = {}, border = BorderStroke(1.dp , if (checkState[index]) Color.Red else Color.Black)) {
+                    Text(text = btnTexts[index])
+                    Icon(imageVector = if(checkState[index]) Icons.Default.Check else Icons.Outlined.Check , contentDescription = null )
+                }
+            }
+        }
+    }
+}
+@Composable
+fun RegularlySpend(){
+    val checkState = remember {
+        mutableStateListOf(false,false,false,false,false)
+    }
+    val btnTexts = listOf(
+        "Groceries",
+        "Phone",
+        "Internet",
+        "Personal Care",
+        "Clothing"
+    )
+    Column {
+        Text("Which of these do you regularly spend money on?")
+        repeat(btnTexts.size){index ->
+            Row{
+                Button(onClick = {}, border = BorderStroke(1.dp , if (checkState[index]) Color.Red else Color.Black)) {
+                    Text(text = btnTexts[index])
+                    Icon(imageVector = if(checkState[index]) Icons.Default.Check else Icons.Outlined.Check , contentDescription = null )
+                }
+            }
+        }
+    }
+}
+@Composable
+fun OverView(){
     Column {
 
     }
