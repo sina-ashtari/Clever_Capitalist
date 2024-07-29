4d7d2dac9d3716bc45186de5e3997f990027a466
Sina <sinaa1383@gmail.com>
2024-07-29 19:10:20 +0330

    Added dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -89,6 +89,7 @@ dependencies {
 
     implementation (libs.firebase.auth)
     implementation (libs.firebase.firestore)
+    implementation (libs.firebase.firestore.ktx)
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.analytics)
 
