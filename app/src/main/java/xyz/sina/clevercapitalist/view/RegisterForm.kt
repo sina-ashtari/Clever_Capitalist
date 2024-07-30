5ca639bfc868a124bf24543e6a81e32d002788cd
Sina <sinaa1383@gmail.com>
2024-07-30 18:56:06 +0330

    fixed bugs

--- app/src/main/java/xyz/sina/clevercapitalist/view/RegisterForm.kt
+++ app/src/main/java/xyz/sina/clevercapitalist/view/RegisterForm.kt
@@ -16,7 +16,9 @@ import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.outlined.Check
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -24,6 +26,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.launch
 
@@ -34,15 +37,23 @@ fun RegisterForm(){
     val scope = rememberCoroutineScope()
     val pagerState = rememberPagerState {5}
 
+    var userName by remember { mutableStateOf("") }
+    var rent by remember { mutableStateOf("") }
+    var transport by remember { mutableStateOf("") }
+    var fees by remember { mutableStateOf("") }
+    var otherExpenses by remember { mutableStateOf("") }
+
     Scaffold(){ innerPadding ->
         Column(modifier = Modifier.padding(innerPadding)) {
             HorizontalPager(state = pagerState) { page: Int ->  
                 when(page){
-                    0 -> {NameGetter()}
-                    1 -> {HomeStatus()}
+                    0 -> {userName = nameGetter() }
+                    1 -> {rent = HomeStatus().toString()
+                    }
                     2 -> {TravelStatus()}
                     3 -> {RegularlySpend()}
-                    4 -> {}
+                    // at this point, you can show info from viewmodel
+                    4 -> {OverView()}
                 }
             }
 
@@ -51,6 +62,7 @@ fun RegisterForm(){
                     Text(text = "Next")
                 }
             }else{
+                // here you should send information
                 Button(onClick = {}) {
                     Text("Good to go!")
                 }
@@ -61,14 +73,15 @@ fun RegisterForm(){
 
 
 @Composable
-fun NameGetter() {
-    val name = remember { mutableStateOf("") }
+fun nameGetter(): String {
+    var name by remember { mutableStateOf("") }
     Column {
 
         Text("What should we call you, clever capitalist ?")
-        OutlinedTextField(value = name.value, onValueChange = {name.value = it} )
+        OutlinedTextField(value = name, onValueChange = {name = it} )
 
     }
+    return name
 }
 @Composable
 fun HomeStatus(){
@@ -78,8 +91,8 @@ fun HomeStatus(){
         "I own",
         "Other"
     )
-    var selectedIndex by remember { mutableStateOf(-1) }
-
+    var selectedIndex by remember { mutableIntStateOf(-1) }
+    var rent by remember { mutableStateOf("") }
 
     Column {
         Text("Tell us about your home")
@@ -91,7 +104,6 @@ fun HomeStatus(){
                     Icon(imageVector = if(selectedIndex == index) Icons.Default.Check else Icons.Outlined.Check , contentDescription = null )
                 }
             }
-
         }
 
     }
--- app/src/main/java/xyz/sina/clevercapitalist/viewModel/registerFormViewModel/RegisterViewModel.kt
+++ app/src/main/java/xyz/sina/clevercapitalist/viewModel/registerFormViewModel/RegisterViewModel.kt
@@ -1,8 +1,12 @@
 package xyz.sina.clevercapitalist.viewModel.registerFormViewModel
 
+import android.widget.Toast
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.google.firebase.firestore.FirebaseFirestore
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import xyz.sina.clevercapitalist.model.RegisterInfo
 import javax.inject.Inject
 
 
@@ -10,5 +14,13 @@ import javax.inject.Inject
 class RegisterViewModel @Inject constructor(
     private val fireStore : FirebaseFirestore
 ): ViewModel() {
+    fun saveRegisterInfo(registerInfo: RegisterInfo){
+        viewModelScope.launch {
+            try {
+                fireStore.collection("register_info").add(registerInfo)
+            }catch (e:Exception){
 
+            }
+        }
+    }
 }
