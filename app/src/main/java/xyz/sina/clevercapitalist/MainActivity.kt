31b5423fbd881925ee070b3913636362fc336ac2
Sina <sinaa1383@gmail.com>
2024-07-31 21:04:03 +0330

    feat : Added to go in dashboard when user is still logged in

--- app/src/main/java/xyz/sina/clevercapitalist/MainActivity.kt
+++ app/src/main/java/xyz/sina/clevercapitalist/MainActivity.kt
@@ -9,6 +9,9 @@ import androidx.navigation.compose.rememberNavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import dagger.hilt.android.AndroidEntryPoint
 import xyz.sina.clevercapitalist.view.MainView
 import xyz.sina.clevercapitalist.view.Routes
@@ -20,17 +23,23 @@ import xyz.sina.clevercapitalist.view.RegisterForm
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private lateinit var auth : FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             CleverCapitalistTheme {
-
-// probably we should have sharedPreference here as we check we logged in before our not.
-
+                auth = Firebase.auth
+                val currentUser = auth.currentUser
                 val navController = rememberNavController()
-                Navigation(navController = navController)
 
+                if(currentUser != null){
+                    Navigation(navController = navController, Routes.REGISTER_FORM)
+                }else{
+                    Navigation(navController = navController,Routes.MAIN_ROUTE )
+                }
             }
         }
     }
@@ -38,12 +47,11 @@ class MainActivity : ComponentActivity() {
 
 // need a welcome screen that goes for signUp or Sign in option, then to the Dashboard screen
 // in signIn Screen we need a Forget password but not for now,
-// should change startDestination if user logged in, but not now !
 
 @Composable
-fun Navigation(navController: NavHostController){
+fun Navigation(navController: NavHostController, startDestination : String){
 
-    NavHost(navController = navController, startDestination = Routes.MAIN_ROUTE){
+    NavHost(navController = navController, startDestination = startDestination){
         composable(Routes.SIGN_IN_ROUTE){ SignInView(navController = navController) }
         composable(Routes.SIGN_UP_ROUTE){ SignUpView(navController = navController) }
         composable(Routes.MAIN_ROUTE){ MainView(navController = navController) }
