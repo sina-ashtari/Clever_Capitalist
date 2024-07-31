661b40783d0ea5c28903c3943026eae253dae9c7
Sina <sinaa1383@gmail.com>
2024-07-31 21:04:46 +0330

    feat : Added FireStore module

---
+++ app/src/main/java/xyz/sina/clevercapitalist/model/FireStoreModule.kt
@@ -0,0 +1,19 @@
+package xyz.sina.clevercapitalist.model
+
+import com.google.firebase.firestore.FirebaseFirestore
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object FireStoreModule {
+    @Provides
+    @Singleton
+    fun provideFireStore(): FirebaseFirestore{
+        return FirebaseFirestore.getInstance()
+    }
+}
