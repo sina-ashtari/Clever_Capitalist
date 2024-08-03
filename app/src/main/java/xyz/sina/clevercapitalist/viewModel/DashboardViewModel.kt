ab3bc313f7dd63d6d461f0c38db63f1f74b0a097
Sina <sinaa1383@gmail.com>
2024-08-03 20:13:05 +0330

    refactor : fixed bugs

--- app/src/main/java/xyz/sina/clevercapitalist/viewModel/DashboardViewModel.kt
+++ app/src/main/java/xyz/sina/clevercapitalist/viewModel/DashboardViewModel.kt
@@ -6,6 +6,8 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import xyz.sina.clevercapitalist.model.FirestoreRepository
 import xyz.sina.clevercapitalist.model.RegisterInfo
@@ -16,16 +18,18 @@ class DashboardViewModel @Inject constructor(
     private val repository: FirestoreRepository
 ): ViewModel(){
 
-    private val _userData = mutableStateOf<Result<RegisterInfo>?>(null)
-    val userData: State<Result<RegisterInfo>?> = _userData
+    private val _data = MutableStateFlow<List<RegisterInfo>>(emptyList())
+    val data : StateFlow<List<RegisterInfo>> = _data
 
-    fun getDataFromFireStore(){
-       viewModelScope.launch {
-           repository.getUserData().collect{ result ->
-               _userData.value = result
+    init {
+        fetchData()
+    }
 
-           }
-       }
+    private fun fetchData(){
+        viewModelScope.launch {
+            val result = repository.getDataFromFireStore()
+            _data.value = result
+        }
     }
 
 }
