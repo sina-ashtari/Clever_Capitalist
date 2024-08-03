5837365c61bb8b1dd2d98f89602c832fc73dba7f
Sina <sinaa1383@gmail.com>
2024-08-03 20:12:49 +0330

    feat : added dashboard ui

--- app/src/main/java/xyz/sina/clevercapitalist/view/DashboardView.kt
+++ app/src/main/java/xyz/sina/clevercapitalist/view/DashboardView.kt
@@ -3,28 +3,36 @@ package xyz.sina.clevercapitalist.view
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.State
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
+import xyz.sina.clevercapitalist.model.RegisterInfo
 import xyz.sina.clevercapitalist.viewModel.DashboardViewModel
+
 
 @Composable
 fun Dashboard(){
-    val viewModel: DashboardViewModel = hiltViewModel()
-    val userData = viewModel.userData.value
 
-    LaunchedEffect(Unit) {
-        viewModel.getDataFromFireStore()
-    }
+    val viewModel : DashboardViewModel = hiltViewModel()
+    val data = viewModel.data.collectAsState()
+    UserUI(data)
 
 }
 
 @Composable
-fun UserUI(){
+fun UserUI(data: State<List<RegisterInfo>>) {
     Scaffold(){innerPadding ->
         Column(modifier = Modifier.padding(innerPadding)) {
-
+            data.value.forEach { item ->
+                Text(text = "hello ${item.userName}!")
+                Text(text = "Your previous debts are : ${item.debts}")
+                Text(text = "Your monthly transport fee : ${item.transport}")
+                Text(text = "Your house rent is : ${item.houseRent}")
+                Text(text = "Your other stuff : ${item.otherExpenses}" )
+            }
         }
     }
 }
