aa72e2ab402d39e566c6e419b4c309861451c050
Sina <sinaa1383@gmail.com>
2024-08-04 17:35:22 +0330

    fix : fixed bug shows all users

--- app/src/main/java/xyz/sina/clevercapitalist/viewModel/registerFormViewModel/RegisterViewModel.kt
+++ app/src/main/java/xyz/sina/clevercapitalist/viewModel/registerFormViewModel/RegisterViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -17,24 +18,30 @@ class RegisterViewModel @Inject constructor(
     private val fireStore : FirebaseFirestore
 ): ViewModel() {
 
+    private val currentUser = FirebaseAuth.getInstance().currentUser
+    private val uid = currentUser?.uid
+
     private var _fireStoreIsSuccess = false
     private var _fireStoreException = ""
     val fireStoreException : String = _fireStoreException
     val fireStoreIsSuccess : Boolean = _fireStoreIsSuccess
 
     fun saveRegisterInfo(registerInfo: RegisterInfo){
-        viewModelScope.launch {
-            try {
-                fireStore.collection("register_info").add(registerInfo).addOnSuccessListener {
-                    _fireStoreIsSuccess = true
-                }.addOnFailureListener { exception ->
-                    _fireStoreIsSuccess = true
-                    _fireStoreException = exception.toString()
-
-                    Log.e("CHECK", "Error is : $exception")
+        if(uid != null){
+            viewModelScope.launch {
+                try {
+                    fireStore.collection("users").document(uid).collection("register_info").add(registerInfo).addOnSuccessListener {
+                        _fireStoreIsSuccess = true
+                    }.addOnFailureListener { exception ->
+                        _fireStoreIsSuccess = false
+                        _fireStoreException = exception.toString()
+                        Log.e("CHECK", "Error is : $exception")
+                    }
+                }catch (e:Exception){
+                    _fireStoreIsSuccess = false
+                    _fireStoreException = e.toString()
+                    Log.e("CHECK","Error is : $e")
                 }
-            }catch (e:Exception){
-                Log.e("CHECK","Error is : $e")
             }
         }
     }
