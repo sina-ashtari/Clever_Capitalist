a8a1ff04ba017d542f2404a0ff97665735bee89d
Sina <sinaa1383@gmail.com>
2024-08-04 17:36:36 +0330

    fix : fixed uid

--- app/src/main/java/xyz/sina/clevercapitalist/viewModel/DashboardViewModel.kt
+++ app/src/main/java/xyz/sina/clevercapitalist/viewModel/DashboardViewModel.kt
@@ -1,10 +1,12 @@
 package xyz.sina.clevercapitalist.viewModel
 
 import androidx.compose.runtime.State
+import androidx.compose.runtime.currentCompositionErrors
 import androidx.compose.runtime.mutableStateOf
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.google.firebase.auth.FirebaseAuth
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -26,9 +28,15 @@ class DashboardViewModel @Inject constructor(
     }
 
     private fun fetchData(){
-        viewModelScope.launch {
-            val result = repository.getDataFromFireStore()
-            _data.value = result
+        val currentUser = FirebaseAuth.getInstance().currentUser
+        val uid = currentUser?.uid
+        if(uid != null){
+            viewModelScope.launch {
+                val result = repository.getDataFromFireStore(uid)
+                _data.value = result
+            }
+        }else{
+            _data.value = emptyList()
         }
     }
 
