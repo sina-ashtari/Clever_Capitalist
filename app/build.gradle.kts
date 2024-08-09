15891c76ac3368f3d4319ac2ef6202c068be0bec
Sina <sinaa1383@gmail.com>
2024-08-09 19:37:47 +0330

    Added dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
 
     defaultConfig {
         applicationId = "xyz.sina.clevercapitalist"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -70,23 +70,28 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation (libs.ui)
+    implementation (libs.androidx.material)
+    implementation (libs.ui.tooling.preview)
+    implementation (libs.androidx.navigation.compose)
 
+    // hilt
     implementation (libs.hilt.android)
     kapt (libs.hilt.compiler)
-
-    // For instrumentation tests
     androidTestImplementation  (libs.hilt.android.testing)
     kaptAndroidTest (libs.hilt.compiler)
-
-    // For local unit tests
     testImplementation (libs.hilt.android.testing)
     kaptTest (libs.hilt.compiler)
+    implementation (libs.androidx.hilt.navigation.compose)
 
+    // retrofit
     implementation (libs.retrofit)
     implementation (libs.converter.gson)
 
+    // realm database
     implementation (libs.realm.gradle.plugin)
 
+    // firebase
     implementation (libs.firebase.auth)
     implementation (libs.firebase.firestore)
     implementation (libs.firebase.firestore.ktx)
@@ -95,21 +100,15 @@ dependencies {
     implementation (libs.play.services.identity)
     implementation (libs.play.services.auth)
 
-
-    implementation (libs.ui)
-    implementation (libs.androidx.material)
-    implementation (libs.ui.tooling.preview)
-    implementation (libs.androidx.navigation.compose)
-    implementation (libs.androidx.hilt.navigation.compose)
-
-
+    // compose viewmodel
     implementation (libs.androidx.lifecycle.viewmodel.ktx)
     implementation (libs.androidx.lifecycle.viewmodel.compose)
-
     implementation (libs.androidx.lifecycle.livedata.ktx)
     implementation (libs.androidx.runtime.livedata)
-
     implementation(libs.androidx.lifecycle.runtime.compose)
+
+    // yChart graph
+    implementation (libs.yml.ycharts)
 }
 kapt {
     correctErrorTypes = true
