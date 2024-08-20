3db8083bd66c15b95f025a9eae85810afdb55490
Sina <sinaa1383@gmail.com>
2024-08-20 20:45:14 +0330

    feat : now you can set financial goals

--- app/src/main/java/xyz/sina/clevercapitalist/view/RegisterForm.kt
+++ app/src/main/java/xyz/sina/clevercapitalist/view/RegisterForm.kt
@@ -8,26 +8,29 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+//noinspection UsingMaterialAndMaterial3Libraries
 import androidx.compose.material.Button
+//noinspection UsingMaterialAndMaterial3Libraries
 import androidx.compose.material.ButtonDefaults
 //noinspection UsingMaterialAndMaterial3Libraries
 import androidx.compose.material.Scaffold
+//noinspection UsingMaterialAndMaterial3Libraries
 import androidx.compose.material.SnackbarHost
+//noinspection UsingMaterialAndMaterial3Libraries
 import androidx.compose.material.SnackbarHostState
+//noinspection UsingMaterialAndMaterial3Libraries
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Clear
-import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
@@ -36,19 +39,29 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
-import com.google.firebase.auth.UserInfo
 import kotlinx.coroutines.launch
 import xyz.sina.clevercapitalist.model.RealmRegisterInfo
 import xyz.sina.clevercapitalist.model.RegisterInfo
 import xyz.sina.clevercapitalist.viewModel.RealmViewModel.RealmViewModel
 import xyz.sina.clevercapitalist.viewModel.registerFormViewModel.RegisterViewModel
 
+data class FinancialGoals(
+    var goal : TextFieldValue = TextFieldValue(""),
+    var moneyForGoal : TextFieldValue = TextFieldValue("")
+
+)
+
+fun FinancialGoals.toMap(): Map<String, Any> {
+    return mapOf(
+        "goal" to goal,
+        "moneyForGoal" to moneyForGoal
+    )
+}
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
@@ -60,8 +73,7 @@ fun RegisterForm(navController: NavHostController){
     val snackBarHostState = remember {SnackbarHostState()}
     val scope = rememberCoroutineScope()
 
-    val goalTextFieldValue = remember{ mutableStateListOf<TextFieldValue>() }
-    val moneyGoalTextFieldValues  = remember { mutableStateListOf<TextFieldValue>()}
+    val financialGoalsList = remember { mutableStateListOf<FinancialGoals>() }
 
     var userName by remember { mutableStateOf("") }
     var salary by remember { mutableStateOf("") }
@@ -73,87 +85,81 @@ fun RegisterForm(navController: NavHostController){
     Scaffold(snackbarHost = {
         SnackbarHost(hostState = snackBarHostState)
     },
-        backgroundColor = MaterialTheme.colorScheme.background
-    ){ innerPadding ->
+        backgroundColor = MaterialTheme.colorScheme.background,
+        bottomBar = {
+            Row(modifier = Modifier
+                .fillMaxWidth()
+                .background(MaterialTheme.colorScheme.surface)
+                .padding(16.dp), horizontalArrangement = Arrangement.Center){
+
+                Button(onClick = {
+                    val userInfo = RegisterInfo(
+                        userName = userName ,
+                        salary = salary.toDoubleOrNull() ?: 0.0,
+                        houseRent = rent.toDoubleOrNull() ?: 0.0,
+                        transport = transport.toDoubleOrNull() ?: 0.0,
+                        debts = debts.toDoubleOrNull() ?: 0.0,
+                        otherExpenses = otherExpenses.toDoubleOrNull() ?: 0.0
+                    )
+
+                    scope.launch {
+                        val realmRegisterInfo = RealmRegisterInfo().apply {
+                            dbUserName = userName
+                            dbSalary = salary.toDoubleOrNull() ?: 0.0
+                            dbHouseRent = rent.toDoubleOrNull() ?: 0.0
+                            dbTransport = transport.toDoubleOrNull() ?: 0.0
+                            dbDebts = debts.toDoubleOrNull() ?: 0.0
+                            dbOtherExpenses = otherExpenses.toDoubleOrNull() ?: 0.0
+                        }
+                        realmViewModel.addRegisterInfo(realmRegisterInfo)
+                    }
+
+                    viewModel.saveRegisterInfo(userInfo,financialGoalsList)
+                    navController.navigate(Routes.DASHBOARD_ROUTE)
+                }
+                    , colors = ButtonDefaults.buttonColors(MaterialTheme.colorScheme.primary)
+                ) {
+                    Text(color = MaterialTheme.colorScheme.onPrimary ,text="Good to go!")
+                }
+            }
+        }
+    ){ _ ->
 
         Column(modifier = Modifier
             .padding(16.dp)
             .verticalScroll(rememberScrollState())
             .fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
-            Spacer(modifier=  Modifier.weight(1f))
             Text(textAlign = TextAlign.Center ,color = MaterialTheme.colorScheme.onBackground ,text="Please enter your name")
             OutlinedTextField(modifier = Modifier.fillMaxWidth(), maxLines = 1 ,value = userName, onValueChange = {userName = it }, label = {Text(color = MaterialTheme.colorScheme.onBackground ,text="Name")})
-            Spacer(modifier=  Modifier.weight(1f))
             Text(textAlign = TextAlign.Center ,color = MaterialTheme.colorScheme.onBackground ,text="Please enter your monthly salary")
             OutlinedTextField(modifier = Modifier.fillMaxWidth() ,value = salary, onValueChange = {salary = it }, label = {Text(color = MaterialTheme.colorScheme.onBackground ,text="Monthly Salary")})
-            Spacer(modifier=  Modifier.weight(1f))
             // IDK now how to implement these in country which everything changes in seconds
             Text(textAlign = TextAlign.Center ,color = MaterialTheme.colorScheme.onBackground ,text="How about your house ?")
             OutlinedTextField(modifier = Modifier.fillMaxWidth() ,value = rent, onValueChange = {rent = it }, label = {Text(color = MaterialTheme.colorScheme.onBackground ,text="Monthly Rent")})
-            Spacer(modifier=  Modifier.weight(1f))
             Text(textAlign = TextAlign.Center ,color = MaterialTheme.colorScheme.onBackground ,text="How much money do you spend to get around?")
             OutlinedTextField(modifier = Modifier.fillMaxWidth() ,value = transport, onValueChange = {transport = it }, label = {Text(color = MaterialTheme.colorScheme.onBackground ,text="Monthly transport")})
-            Spacer(modifier=  Modifier.weight(1f))
             Text(textAlign = TextAlign.Center ,color = MaterialTheme.colorScheme.onBackground ,text="Do you currently have any debt?")
             OutlinedTextField(modifier = Modifier.fillMaxWidth() ,value = debts, onValueChange = {debts = it }, label = {Text(color = MaterialTheme.colorScheme.onBackground ,text="debts")})
-            Spacer(modifier=  Modifier.weight(1f))
             Text(textAlign = TextAlign.Center ,color = MaterialTheme.colorScheme.onBackground ,text="How much money do you spend on other things like Internet, Phone, Groceries , etc.?")
             OutlinedTextField(modifier = Modifier.fillMaxWidth() ,value = otherExpenses, onValueChange = {otherExpenses = it }, label = {Text(color = MaterialTheme.colorScheme.onBackground ,text="Other expenses")})
-            Spacer(modifier=  Modifier.weight(1f))
-            Button(colors = ButtonDefaults.buttonColors(MaterialTheme.colorScheme.primary),
+            Button(modifier = Modifier.padding(top = 16.dp, bottom = 16.dp),colors = ButtonDefaults.buttonColors(MaterialTheme.colorScheme.primary),
                 onClick = {
-                    goalTextFieldValue.add(TextFieldValue(""))
-                    moneyGoalTextFieldValues.add(TextFieldValue(""))
+                    financialGoalsList.add(FinancialGoals())
                 }){
                 Text(color = MaterialTheme.colorScheme.onPrimary,text = "Do you have any financial goal? if do, click me!")
             }
-            goalTextFieldValue.forEachIndexed { index, goalTextFieldValueValue ->
-                Row(modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically){
-                    moneyGoalTextFieldValues.forEachIndexed { _ , moneyGoalTextFieldValue ->
-                        OutlinedTextField(value = goalTextFieldValueValue , onValueChange = {newValue -> goalTextFieldValue[index] = newValue }, label = {Text(color = MaterialTheme.colorScheme.onBackground ,text="Goal ${index+1}")})
-                        OutlinedTextField(value = moneyGoalTextFieldValue , onValueChange = {newValue -> goalTextFieldValue[index] = newValue }, label = {Text(color = MaterialTheme.colorScheme.onBackground ,text="Money ${index+1}")})
-                        IconButton(
-                            onClick = {
-                                goalTextFieldValue.removeAt(index)
-                                moneyGoalTextFieldValues.removeAt(index)
-                            }
-                        ) {
-                            Icon(tint = MaterialTheme.colorScheme.onBackground,imageVector = Icons.Default.Clear, contentDescription = null)
-                        }
+            financialGoalsList.forEachIndexed { index , financialGoals ->
+                Row(modifier = Modifier.fillMaxWidth() , verticalAlignment = Alignment.CenterVertically){
+                    OutlinedTextField(modifier = Modifier.weight(1f),value = financialGoals.goal, label = {Text(color = MaterialTheme.colorScheme.onBackground, text = "Goal ${index+1}")} ,onValueChange = {newValue -> financialGoalsList[index] = financialGoalsList[index].copy(goal = newValue)})
+                    Spacer(modifier = Modifier.width(14.dp))
+                    OutlinedTextField(modifier = Modifier.weight(1f),value = financialGoals.moneyForGoal, label = {Text(color = MaterialTheme.colorScheme.onBackground, text = "Money")},onValueChange = {newValue -> financialGoalsList[index] = financialGoalsList[index].copy(moneyForGoal = newValue)})
+                    IconButton(onClick = {
+                        financialGoalsList.removeAt(index)
+                    }) {
+                        Icon(imageVector = Icons.Default.Clear,tint  = MaterialTheme.colorScheme.onBackground, contentDescription = null)
                     }
                 }
             }
-
-            Button(onClick = {
-                val userInfo = RegisterInfo(
-                    userName = userName ,
-                    salary = salary.toDoubleOrNull() ?: 0.0,
-                    houseRent = rent.toDoubleOrNull() ?: 0.0,
-                    transport = transport.toDoubleOrNull() ?: 0.0,
-                    debts = debts.toDoubleOrNull() ?: 0.0,
-                    otherExpenses = otherExpenses.toDoubleOrNull() ?: 0.0
-                )
-
-                scope.launch {
-                    val realmRegisterInfo = RealmRegisterInfo().apply {
-                        dbUserName = userName
-                        dbSalary = salary.toDoubleOrNull() ?: 0.0
-                        dbHouseRent = rent.toDoubleOrNull() ?: 0.0
-                        dbTransport = transport.toDoubleOrNull() ?: 0.0
-                        dbDebts = debts.toDoubleOrNull() ?: 0.0
-                        dbOtherExpenses = otherExpenses.toDoubleOrNull() ?: 0.0
-                    }
-                    realmViewModel.addRegisterInfo(realmRegisterInfo)
-                }
-
-                viewModel.saveRegisterInfo(userInfo)
-                navController.navigate(Routes.DASHBOARD_ROUTE)
-            }
-            , colors = ButtonDefaults.buttonColors(MaterialTheme.colorScheme.primary)
-            ) {
-                Text(color = MaterialTheme.colorScheme.onPrimary ,text="Good to go!")
-            }
-            Spacer(modifier=  Modifier.weight(1f))
         }
     }
 }
--- app/src/main/java/xyz/sina/clevercapitalist/viewModel/registerFormViewModel/RegisterViewModel.kt
+++ app/src/main/java/xyz/sina/clevercapitalist/viewModel/registerFormViewModel/RegisterViewModel.kt
@@ -10,8 +10,16 @@ import com.google.firebase.firestore.FirebaseFirestore
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import xyz.sina.clevercapitalist.model.RegisterInfo
+import xyz.sina.clevercapitalist.view.FinancialGoals
+import xyz.sina.clevercapitalist.view.toMap
 import javax.inject.Inject
 
+fun FinancialGoals.toMap(): Map<String, Any> {
+    return mapOf(
+        "goal" to goal,
+        "moneyForGoal" to moneyForGoal
+    )
+}
 
 @HiltViewModel
 class RegisterViewModel @Inject constructor(
@@ -21,25 +29,27 @@ class RegisterViewModel @Inject constructor(
     private val currentUser = FirebaseAuth.getInstance().currentUser
     private val uid = currentUser?.uid
 
-    private var _fireStoreIsSuccess = false
-    private var _fireStoreException = ""
-    val fireStoreException : String = _fireStoreException
-    val fireStoreIsSuccess : Boolean = _fireStoreIsSuccess
 
-    fun saveRegisterInfo(registerInfo: RegisterInfo){
+    fun saveRegisterInfo(registerInfo: RegisterInfo,financialGoalsList: List<FinancialGoals>){
         if(uid != null){
             viewModelScope.launch {
                 try {
-                    fireStore.collection("users").document(uid).collection("register_info").add(registerInfo).addOnSuccessListener {
-                        _fireStoreIsSuccess = true
-                    }.addOnFailureListener { exception ->
-                        _fireStoreIsSuccess = false
-                        _fireStoreException = exception.toString()
+                    financialGoalsList.forEach {
+                        val financialGoalsMap = it.toMap()
+                        fireStore.collection("users").document(uid).collection("goals").add(financialGoalsMap).addOnFailureListener {exception->
+                            Log.e("CHECK","Error is : $exception")
+                        }
+                    }
+                }catch (e:Exception){
+                    Log.e("CHECK","Error is : $e")
+                }
+            }
+            viewModelScope.launch {
+                try {
+                    fireStore.collection("users").document(uid).collection("register_info").add(registerInfo).addOnFailureListener { exception ->
                         Log.e("CHECK", "Error is : $exception")
                     }
                 }catch (e:Exception){
-                    _fireStoreIsSuccess = false
-                    _fireStoreException = e.toString()
                     Log.e("CHECK","Error is : $e")
                 }
             }
