dc7e9c049b8dcfbee3cf156f88b00a10e3a90d24
Sina <sinaa1383@gmail.com>
2024-08-21 15:37:28 +0330

    getting goals now available

--- app/src/main/java/xyz/sina/clevercapitalist/model/Authentication/FirestoreRepository.kt
+++ app/src/main/java/xyz/sina/clevercapitalist/model/Authentication/FirestoreRepository.kt
@@ -1,15 +1,29 @@
 package xyz.sina.clevercapitalist.model.Authentication
 
+import android.util.Log
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.toObject
 import kotlinx.coroutines.tasks.await
 import xyz.sina.clevercapitalist.model.RegisterInfo
+import xyz.sina.clevercapitalist.view.FinancialGoals
+import xyz.sina.clevercapitalist.view.toMap
 
 class FirestoreRepository(private val db: FirebaseFirestore){
     suspend fun getDataFromFireStore(uid: String): List<RegisterInfo>{
         return try{
-            val snapshot =db.collection("users").document(uid).collection("register_info").get().await()
+            val snapshot = db.collection("users").document(uid).collection("register_info").get().await()
             snapshot.documents.mapNotNull { document ->
                 document.toObject(RegisterInfo::class.java)
+            }
+        }catch (e:Exception){
+            emptyList()
+        }
+    }
+    suspend fun getGoalsFromFireStore(uid : String):List<FinancialGoals>{
+        return try{
+            val snapshot = db.collection("users").document(uid).collection("goals").get().await()
+            snapshot.documents.mapNotNull { document ->
+                document.toObject(FinancialGoals::class.java)
             }
         }catch (e:Exception){
             emptyList()
--- app/src/main/java/xyz/sina/clevercapitalist/viewModel/DashboardViewModel.kt
+++ app/src/main/java/xyz/sina/clevercapitalist/viewModel/DashboardViewModel.kt
@@ -1,5 +1,6 @@
 package xyz.sina.clevercapitalist.viewModel
 
+import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.google.firebase.auth.FirebaseAuth
@@ -9,6 +10,7 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import xyz.sina.clevercapitalist.model.Authentication.FirestoreRepository
 import xyz.sina.clevercapitalist.model.RegisterInfo
+import xyz.sina.clevercapitalist.view.FinancialGoals
 import javax.inject.Inject
 
 @HiltViewModel
@@ -18,6 +20,8 @@ class DashboardViewModel @Inject constructor(
 
     private val _data = MutableStateFlow<List<RegisterInfo>>(emptyList())
     val data : StateFlow<List<RegisterInfo>> = _data
+
+    val financialGoalsList = mutableStateListOf<FinancialGoals>()
 
     init {
         fetchData()
@@ -30,6 +34,11 @@ class DashboardViewModel @Inject constructor(
             viewModelScope.launch {
                 val result = repository.getDataFromFireStore(uid)
                 _data.value = result
+            }
+            viewModelScope.launch {
+                val goals = repository.getGoalsFromFireStore(uid)
+                financialGoalsList.clear()
+                financialGoalsList.addAll(goals)
             }
         }else{
             _data.value = emptyList()
